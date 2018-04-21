aafd4a376c5d8daabf0fb209fac647203076c857
ssmith-wombatweb <ssmith@wombatweb.us>
2018-04-21 16:49:34 -0700

    Added Under Construction to in progress pages.

--- src/pages/make-contact.jsx
+++ src/pages/make-contact.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const MakeContact = () => (
   <div>
     <h1>Make Contact</h1>
+    <h2>UNDER CONSTRUCTION</h2>
     <p>Interested in hiring me or have questions about my work? Please feel free to contact me.</p>
   </div>
 );
--- src/pages/who-am-i.jsx
+++ src/pages/who-am-i.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const WhoAmI = () => (
   <div>
     <h1>Who Am I?</h1>
+    <h2>UNDER CONSTRUCTION</h2>
     <p>I am Simeon Smith.</p>
   </div>
 );

fd2e824b8d7a7ae073862964b609db1f3a3fdf32
ssmith-wombatweb <ssmith@wombatweb.us>
2018-04-21 16:54:27 -0700

    Built and sent to gh-pages.

--- src/pages/my-work.jsx
+++ src/pages/my-work.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MyWork = () => (
   <div>
     <h1>My Work</h1>
-    <h2>UNDER CONSTRUCTION</h2>
+    <h2 className="sub-heading">UNDER CONSTRUCTION</h2>
     <p>Check out my work below.</p>
   </div>
 );
--- src/pages/who-am-i.jsx
+++ src/pages/who-am-i.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const WhoAmI = () => (
   <div>
     <h1>Who Am I?</h1>
-    <h2>UNDER CONSTRUCTION</h2>
+    <h2 className="sub-heading">UNDER CONSTRUCTION</h2>
     <p>I am Simeon Smith.</p>
   </div>
 );

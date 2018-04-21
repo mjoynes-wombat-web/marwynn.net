903bd6e65a8c2f48260fa564c3e87b7db12f7a6e
ssmith-wombatweb <ssmith@wombatweb.us>
2018-04-21 10:12:30 -0700

    Basic menu layout.

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,27 +1,69 @@
 import React from 'react';
 import Link from 'gatsby-link';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const Menu = ({ pages }) => (
-  <nav id="mainMenu">
+import '../consts/colors.scss';
+
+const Menu = ({ className }) => (
+  <nav className={className} id="mainMenu">
     <ul>
       <li>
         <Link to="/">Home</Link>
       </li>
-      <li><Link to="/my-work/">My Work</Link></li>
-      <li><Link to="/who-am-i/">Who Am I?</Link></li>
-      <li><Link to="/make-contact/">Make Contact</Link></li>
-      <li><Link to="/coding-with-kids/">Coding With Kids</Link></li>
+      <li className="slash">/</li>
+      <li>
+        <Link to="/my-work/">My Work</Link>
+      </li>
+      <li className="slash">/</li>
+      <li>
+        <Link to="/who-am-i/">Who Am I?</Link>
+      </li>
+      <li className="slash">/</li>
+      <li>
+        <Link to="/make-contact/">Make Contact</Link>
+      </li>
+      <li className="slash">/</li>
+      <li>
+        <Link to="/coding-with-kids/">Coding With Kids</Link>
+      </li>
     </ul>
   </nav>
 );
 
-export default Menu;
+export default styled(Menu)`
+  bottom: 0;
+  box-sizing: border-box;
+  padding: 2.5rem;
+  position: absolute;
+  width: 100%;
+
+  ul {
+    display: flex;
+    list-style: none;
+    padding: 0;
+    justify-content: space-around;
+
+    li {
+      display: flex;
+      font-size: 2rem;
+      
+      &.slash {
+        font-size: 2.25rem;
+        font-weight: bold;
+      }
+
+      a {
+        text-decoration: none;
+      }
+    }
+  }
+`;
 
 Menu.propTypes = {
-  pages: PropTypes.arrayOf(PropTypes.string),
+  className: PropTypes.string,
 };
 
 Menu.defaultProps = {
-  pages: [],
+  className: '',
 };
--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
 import Menu from '../components/menu';
-import './index.css';
 
 const Layout = ({ children, data }) => (
   <div>

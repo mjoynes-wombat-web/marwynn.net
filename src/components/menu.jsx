1745e6e38d3270bf78c1b7ab45a7d54295cc1f69
ssmith-wombatweb <ssmith@wombatweb.us>
2018-04-21 12:35:50 -0700

    Completed general menu layout.

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -3,12 +3,12 @@ import Link from 'gatsby-link';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import '../consts/colors.scss';
+import colors from '../consts/colors';
 
-const Menu = ({ className }) => (
+const unstyledMenu = ({ className }) => (
   <nav className={className} id="mainMenu">
     <ul>
-      <li>
+      <li className="active">
         <Link to="/">Home</Link>
       </li>
       <li className="slash">/</li>
@@ -31,39 +31,60 @@ const Menu = ({ className }) => (
   </nav>
 );
 
-export default styled(Menu)`
-  bottom: 0;
-  box-sizing: border-box;
-  padding: 2.5rem 15%;
-  position: absolute;
-  width: 100%;
-
-  ul {
-    display: flex;
-    list-style: none;
-    padding: 0;
-    justify-content: space-around;
-
-    li {
-      display: flex;
-      font-size: 1.5rem;
-      
-      &.slash {
-        font-size: 2rem;
-        font-weight: bold;
-      }
-
-      a {
-        text-decoration: none;
-      }
-    }
-  }
-`;
-
-Menu.propTypes = {
+unstyledMenu.propTypes = {
   className: PropTypes.string,
 };
 
-Menu.defaultProps = {
+unstyledMenu.defaultProps = {
   className: '',
 };
+
+
+const Menu = styled(unstyledMenu)`
+bottom: 0;
+box-sizing: border-box;
+padding: 2rem 8%;
+position: absolute;
+width: 100%;
+
+ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0;
+
+  li {
+    display: flex;
+    font-size: 2rem;
+    margin: 0;
+    color: ${colors.lilacBright};
+    font-family: 'Josefin Sans', 'Open Sans', 'Arial', sans-serif;
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep};
+    transition: all 0.5s;
+    transition-timing-function: cubic-bezier(0.29, -0.69, 0.49, 1.46);
+    
+    &.slash {
+      font-weight: bold;
+      cursor: default;
+    }
+
+    &.active {
+      font-weight: 400;
+    }
+
+    a, a:link, a:visited, a:active, a:focus {
+      text-decoration: none;
+      color: ${colors.lilacBright};
+    }
+
+    &:hover:not(.slash) {
+      transform: scale(1.0625);
+      text-shadow: 0 0 0.75rem ${colors.lilacDeep};
+    }
+  }
+}
+`;
+
+export default Menu;
---
+++ src/consts/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  spring: 'rgb(33, 255, 148)',
+  springLight: 'rgb(148, 255, 203)',
+  tomato: 'rgb(255, 89, 89)',
+  lilac: 'rgb(252, 99, 255)',
+  lilacDeep: 'rgb(249, 66, 255)',
+  lilacBright: 'rgb(255, 249, 255)',
+  carolina: 'rgb(54, 182, 255)',
+  navy: 'rgb(1, 14, 23)',
+};
+
+export default colors;

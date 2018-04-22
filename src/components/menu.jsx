0482de66f89dce229685a60b00ba8a859248afd8
ssmith-wombatweb <ssmith@wombatweb.us>
2018-04-22 15:00:36 -0700

    Setup menu and background animations.

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -5,31 +5,67 @@ import styled from 'styled-components';
 
 import colors from '../consts/colors';
 
-const UnstyledMenu = ({ className }) => (
-  <nav className={className} id="mainMenu">
-    <ul>
-      <li className="active">
-        <Link to="/">Home</Link>
-      </li>
-      <li className="slash">/</li>
-      <li>
-        <Link to="/my-work/">My Work</Link>
-      </li>
-      <li className="slash">/</li>
-      <li>
-        <Link to="/who-am-i/">Who Am I?</Link>
-      </li>
-      <li className="slash">/</li>
-      <li>
-        <Link to="/make-contact/">Make Contact</Link>
-      </li>
-      <li className="slash">/</li>
-      <li>
-        <Link to="/coding-with-kids/">Coding With Kids</Link>
-      </li>
-    </ul>
-  </nav>
-);
+function animateMenu() {
+  const menuItems = document.querySelectorAll('#mainMenu li');
+  const bgImg = document.getElementById('background');
+  const bgGradient = document.querySelector('.background-gradient');
+  const durations = [];
+
+  menuItems.forEach((menuItem) => {
+    const duration = Math.random() * 2000;
+    durations.push(duration);
+    menuItem.classList.add('turnOff');
+    bgImg.classList.add('turnOff');
+
+    setTimeout(() => {
+      menuItem.classList.remove('turnOff');
+      menuItem.classList.add('turnOn');
+    }, duration);
+  });
+  durations.sort((a, b) => a - b);
+
+  setTimeout(() => {
+    bgImg.classList.remove('turnOff');
+    bgImg.classList.add('turnOn');
+  }, durations[0]);
+}
+
+class UnstyledMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+  componentDidMount() {
+    animateMenu();
+  }
+  render() {
+    return (
+      <nav className={this.props.className} id="mainMenu">
+        <ul>
+          <li className="active">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="slash">/</li>
+          <li>
+            <Link to="/my-work/">My Work</Link>
+          </li>
+          {/* <li className="slash">/</li>
+          <li>
+            <Link to="/who-am-i/">Who Am I?</Link>
+          </li> */}
+          <li className="slash">/</li>
+          <li>
+            <Link to="/make-contact/">Make Contact</Link>
+          </li>
+          <li className="slash">/</li>
+          <li>
+            <Link to="/coding-with-kids/">Coding With Kids</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 UnstyledMenu.propTypes = {
   className: PropTypes.string,
@@ -41,6 +77,54 @@ UnstyledMenu.defaultProps = {
 
 
 const Menu = styled(UnstyledMenu)`
+@keyframes fluorescentOn {
+  0%  {
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep(0.65)};
+    color: ${colors.lilacBright(0.25)};
+    transition: none;
+  }
+  14% {
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep(0.65)};
+    color: ${colors.lilacBright(0.25)};
+  }
+  18% {
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep()};
+    color: ${colors.lilacBright()};
+  }
+  20% {
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep(0.65)};
+    color: ${colors.lilacBright(0.25)};
+  }
+  28% {
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep()};
+    color: ${colors.lilacBright()};
+  }
+  30% {
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep(0.65)};
+    color: ${colors.lilacBright(0.25)};
+  }
+  38% {
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep(0.65)};
+    color: ${colors.lilacBright(0.25)};
+  }
+  45% {
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep()};
+    color: ${colors.lilacBright()};
+  }
+  50% {
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep()};
+    color: ${colors.lilacBright()};
+  }
+  52% {
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep(0.65)};
+    color: ${colors.lilacBright(0.25)};
+  }
+  100% {
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep()};
+    transition: all 0.5s;
+  }
+}
+
 bottom: 0;
 box-sizing: border-box;
 padding: 2rem 8%;
@@ -59,22 +143,18 @@ ul {
     display: flex;
     font-size: 2rem;
     margin: 0;
-    color: ${colors.lilacBright};
+    color: ${colors.lilacBright()};
     font-family: 'Josefin Sans', 'Open Sans', 'Arial', sans-serif;
-    text-shadow: 0 0 0.125rem ${colors.lilacDeep};
+    text-shadow: 0 0 0.125rem ${colors.lilacDeep()};
     transition: all 0.5s;
     transition-timing-function: cubic-bezier(0.29, -0.69, 0.49, 1.46);
 
-    @media screen and (max-width: 1200px) {
+    @media screen and (max-width: 1100px) {
       font-size: 1.75rem;
     }
 
-    @media screen and (max-width: 1000px) {
+    @media screen and (max-width: 900px) {
       font-size: 1.5rem;
-    }
-
-    @media screen and (max-width: 850px) {
-      font-size: 1.25rem;
     }
 
     @media screen and (max-width: 700px) {
@@ -92,12 +172,33 @@ ul {
 
     a, a:link, a:visited, a:active, a:focus {
       text-decoration: none;
-      color: ${colors.lilacBright};
+      color: ${colors.lilacBright()};
     }
 
     &:hover:not(.slash) {
       transform: scale(1.0625);
-      text-shadow: 0 0 0.75rem ${colors.lilacDeep};
+      text-shadow: 0 0 0.75rem ${colors.lilacDeep()};
+    }
+
+    &.turnOn {
+      animation-name: fluorescentOn;
+      animation-duration: 2.5s;
+
+      a, a:link, a:visited, a:active, a:focus {
+        animation-name: fluorescentOn;
+        animation-duration: 2.5s;
+      }
+    }
+
+    &.turnOff {
+      text-shadow: 0 0 0.125rem ${colors.lilacDeep(0.65)};
+      color: ${colors.lilacBright(0.25)};
+      transition: none;
+
+      a, a:link, a:visited, a:active, a:focus {
+        text-shadow: 0 0 0.125rem ${colors.lilacDeep(0.65)};
+        color: ${colors.lilacBright(0.25)};
+      }
     }
   }
 }
--- src/consts/colors.js
+++ src/consts/colors.js
@@ -1,12 +1,16 @@
+function rgba(r, g, b, opacity) {
+  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+}
+
 const colors = {
-  spring: 'rgb(33, 255, 148)',
-  springLight: 'rgb(148, 255, 203)',
-  tomato: 'rgb(255, 89, 89)',
-  lilac: 'rgb(252, 99, 255)',
-  lilacDeep: 'rgb(249, 66, 255)',
-  lilacBright: 'rgb(255, 249, 255)',
-  carolina: 'rgb(54, 182, 255)',
-  navy: 'rgb(1, 14, 23)',
+  spring: (opacity = 1) => rgba(33, 255, 148, opacity),
+  springLight: (opacity = 1) => rgba(148, 255, 203, opacity),
+  tomato: (opacity = 1) => rgba(255, 89, 89, opacity),
+  lilac: (opacity = 1) => rgba(252, 99, 255, opacity),
+  lilacDeep: (opacity = 1) => rgba(249, 66, 255, opacity),
+  lilacBright: (opacity = 1) => rgba(255, 249, 255, opacity),
+  carolina: (opacity = 1) => rgba(54, 182, 255, opacity),
+  navy: (opacity = 1) => rgba(1, 14, 23, opacity),
 };
 
 export default colors;

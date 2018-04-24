516c175973ae10fbfa285670d854a12031f0aec1
ssmith-wombatweb <ssmith@wombatweb.us>
2018-04-23 20:26:07 -0700

    Added feedback to contact form.

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -3,6 +3,24 @@ import styled from 'styled-components';
 import colors from '../consts/colors';
 
 const Button = styled.button`
+
+  @keyframes pulse {
+    0% {
+      background-color: ${colors.lilacBright()};
+      box-shadow: inset 0 0 0.75rem ${colors.lilacDeep()},
+      0 0 0.75rem ${colors.lilac(1)};
+    }
+    50% {
+      background-color: ${colors.lilacBright(0.75)};
+      box-shadow: inset 0 0 0.5rem ${colors.lilacDeep(0.65)};
+    }
+
+    100% {
+      background-color: ${colors.lilacBright()};
+      box-shadow: inset 0 0 0.75rem ${colors.lilacDeep()},
+      0 0 0.75rem ${colors.lilac(1)};
+    }
+  }
   font-family: 'Josefin Sans', 'Arial', sans-serif;
   font-weight: 400;
   cursor: pointer;
@@ -20,6 +38,12 @@ const Button = styled.button`
   padding: 1rem 0 0.75rem 0;
   transition: all 0.5s;
   width: 100%;
+
+  &.processing {
+    animation-name: pulse;
+    animation-duration: 2s;
+    animation-iteration-count: infinite;
+  }
 
   @media screen and (max-width: 1700px) {
     font-size: 1.25rem;

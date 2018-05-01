74d1da46075a5caefa8ea67014a9a27e545d2074
ssmith-wombatweb <ssmith@wombatweb.us>
2018-04-30 18:13:52 -0700

    Added click on dots to change image and swipe to change image functionality.

--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -6,21 +6,24 @@ import faArrowCircleRight from '@fortawesome/fontawesome-free-solid/faArrowCircl
 import faArrowCircleLeft from '@fortawesome/fontawesome-free-solid/faArrowCircleLeft';
 
 import colors from '../consts/colors';
+import { ENGINE_METHOD_DIGESTS } from 'constants';
 
 const UnstyledImgBullets = ({
   imgs,
   activeImg,
   projectId,
   className,
+  onClick,
 }) => (
   <nav className={className} >
     {imgs.map(img => (
-      <span
+      <button
         className={activeImg === img.id ? 'active' : null}
-        key={`project${projectId}img${img.id}`}
+        key={`project${projectId}img${img.id}bullet`}
+        onClick={e => onClick(e, img.id)}
       >
         •
-      </span>
+      </button>
     ))}
   </nav>
 );
@@ -33,6 +36,7 @@ UnstyledImgBullets.propTypes = {
     url: PropTypes.string,
   })).isRequired,
   className: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
 };
 
 UnstyledImgBullets.defaultProps = {
@@ -44,12 +48,15 @@ const ImgBullets = styled(UnstyledImgBullets)`
   bottom: -0.125rem;
   z-index: 1;
 
-  span {
+  button {
     color: ${colors.lilacBright(0.5)};
     transition: all 0.5s;
     font-size: 1.5rem;
     line-height: 0;
     font-family: 'Josefin Slab', 'Arial', sans-serif;
+    border: none;
+    padding: 0;
+    outline: none;
 
     &.active {
       color: ${colors.lilacBright(1)};
@@ -107,8 +114,8 @@ const ImgButtons = styled(UnstyledImgButtons)`
     .fa-arrow-circle-left, .fa-arrow-circle-right {
       color: transparent;
       filter: none;
-      width: 20px;
-      height: 19px;
+      width: 25px;
+      height: 24px;
       transition: all 0.5s;
       cursor: pointer;
 
@@ -146,6 +153,10 @@ class UnstyledProjectCard extends React.Component {
     };
 
     this.changeImg = this.changeImg.bind(this);
+    this.selectImg = this.selectImg.bind(this);
+    this.swipeImgStart = this.swipeImgStart.bind(this);
+    this.swipeImgMove = this.swipeImgMove.bind(this);
+    this.swipeImgEnd = this.swipeImgEnd.bind(this);
   }
 
   changeImg(e) {
@@ -160,26 +171,100 @@ class UnstyledProjectCard extends React.Component {
     return this.setState({ activeImg });
   }
 
+  selectImg(e, id) {
+    return this.setState({ activeImg: id });
+  }
+
+  swipeImgStart(e) {
+    this.setState({ lastSwipedX: e.changedTouches[0].clientX });
+  }
+
+  swipeImgMove(e) {
+    const swipedImg = e.currentTarget;
+    const movement = e.changedTouches[0].clientX - this.state.lastSwipedX;
+
+    if (Math.abs(movement) > 50) {
+      const currentImgKey = e.currentTarget.dataset.key;
+      const nextImgId = movement < 0 ? this.state.activeImg - 1 : this.state.activeImg + 1;
+      const beginningKey = currentImgKey.substr(0, currentImgKey.length - 1);
+      let nextImg;
+
+      console.log(`${nextImgId}`);
+
+      if (nextImgId < 0) {
+        nextImg = document.querySelector(`[data-key=${beginningKey}${this.state.imgs.length - 1}]`);
+      } else if (nextImgId > this.state.imgs.length - 1) {
+        nextImg = document.querySelector(`[data-key=${beginningKey}0]`);
+      } else {
+        nextImg = document.querySelector(`[data-key=${beginningKey}${nextImgId}]`);
+      }
+      nextImg.style.opacity = '0.85';
+      nextImg.style.zIndex = '1';
+    }
+    swipedImg.style.transform = `translate(${movement}px, 0)`;
+  }
+
+  swipeImgEnd(e) {
+    const swipedImg = e.currentTarget;
+    if (this.state.lastSwipedX) {
+      const movement = e.changedTouches[0].clientX - this.state.lastSwipedX;
+
+      if (Math.abs(movement) < 50) {
+        swipedImg.style.transform = null;
+        return null;
+      }
+
+      let { activeImg } = this.state;
+
+      if (activeImg > 0 && movement < 0) {
+        activeImg -= 1;
+      } else if (activeImg + 1 < this.state.imgs.length && movement > 0) {
+        activeImg += 1;
+      } else if (movement < 0) {
+        activeImg = this.state.imgs.length - 1;
+      } else if (movement > 0) {
+        activeImg = 0;
+      }
+      const newActiveImg = document.querySelector(`[data-key=${swipedImg.dataset.key.substr(0, swipedImg.dataset.key.length - 1)}${activeImg}]`);
+      newActiveImg.style.opacity = null;
+      newActiveImg.style.zIndex = null;
+      this.setState({ lastSwipedX: null, activeImg });
+    }
+    swipedImg.style.transform = null;
+    return null;
+  }
+
   render() {
     return (
       <article className={`project-card ${this.props.className}`}>
-        <div className="image-wrapper">
-          {this.props.imgs.length > 1
+        <div
+          className="image-wrapper"
+        >
+          {this.state.imgs.length > 1
             ? <ImgButtons
               onClick={this.changeImg}
               activeImg={this.state.activeImg}
               imgsLength={this.state.imgs.length}
             />
             : null}
-          <img
-            src={`https://res.cloudinary.com/design-bright/image/upload//q_auto:good,c_limit,w_300,h_250/v1524792633/portfolio/${this.state.imgs[this.state.activeImg].url}`}
-            alt={this.props.title}
-          />
-          {this.props.imgs.length > 1
+          {this.state.imgs.map(img => (
+            <img
+              data-key={`project${this.props.projectId}img${img.id}`}
+              key={`project${this.props.projectId}img${img.id}`}
+              className={img.id === this.state.activeImg ? 'active' : ''}
+              src={`https://res.cloudinary.com/design-bright/image/upload/c_limit,w_300,h_250/v1524792633/portfolio/${img.url}`}
+              alt={this.props.title}
+              onTouchStart={this.swipeImgStart}
+              onTouchMove={this.swipeImgMove}
+              onTouchEnd={this.swipeImgEnd}
+            />
+          ))}
+          {this.state.imgs.length > 1
             ? <ImgBullets
-              imgs={this.props.imgs}
+              imgs={this.state.imgs}
               activeImg={this.state.activeImg}
               projectId={this.props.projectId}
+              onClick={this.selectImg}
             />
             : null}
         </div>
@@ -220,21 +305,6 @@ const ProjectCard = styled(UnstyledProjectCard)`
   overflow: hidden;
   margin: 1rem 0;
 
-  .image-wrapper:hover {
-    button {
-      :disabled {
-        .fa-arrow-circle-left, .fa-arrow-circle-right {
-          color: ${colors.navy(0.25)};
-          filter: drop-shadow(0 0 0.125rem ${colors.navy(0.75)});
-        }
-      }
-      .fa-arrow-circle-left, .fa-arrow-circle-right {
-        color: ${colors.spring(0.75)};
-        filter: drop-shadow(0 0 0.125rem ${colors.navy(0.75)});
-      }
-    }
-  }
-
   @media screen and (max-width: 1500px) {
     width: calc(33.3333% - 1.3333rem);
   }
@@ -255,12 +325,41 @@ const ProjectCard = styled(UnstyledProjectCard)`
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    padding: 1rem;
+    padding: 1rem 1rem 1.25rem 1rem;
     position: relative;
 
+    &:hover {
+      button {
+        z-index: 10;
+        :disabled {
+          .fa-arrow-circle-left, .fa-arrow-circle-right {
+            color: ${colors.navy(0.25)};
+            filter: drop-shadow(0 0 0.125rem ${colors.navy(0.75)});
+          }
+        }
+        .fa-arrow-circle-left, .fa-arrow-circle-right {
+          color: ${colors.spring(0.75)};
+          filter: drop-shadow(0 0 0.125rem ${colors.navy(0.75)});
+        }
+      }
+    }
+
     img {
-      max-width: 100%;
+      max-width: calc(100% - 2rem);
       border-radius: 0.25rem;
+      position: absolute;
+      opacity: 0.35;
+      transform: translate(0.25rem, 0.25rem);
+      transition: transform 0.25s, opacity 0.25s;
+
+      &.active {
+        max-width: 100%;
+        z-index: 2;
+        opacity: 1;
+        margin: 0;
+        position: relative;
+        transform: none;
+      }
     }
   }
 

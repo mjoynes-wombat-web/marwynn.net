4de1c30448b6ad543d1949bdedc4dfac2ed1890b
ssmith-wombatweb <ssmith@wombatweb.us>
2018-05-01 21:05:53 -0700

    Modified text on 404 page.

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn't exist... the sadness.</p>
+    <h1>Page Not Found</h1>
+    <p>
+      Oh No! You've wondered off and gotten lost. Please use choose from the pages in the menu to
+      find your way back.
+    </p>
   </div>
 );
 

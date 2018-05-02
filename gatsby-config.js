0995f8c93267345c3c44e945097b599f087e84a3
ssmith-wombatweb <ssmith@wombatweb.us>
2018-05-01 17:07:49 -0700

    Built and sent to gh-pages.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,24 @@
+const generateBabelConfig = require('gatsby/dist/utils/babel-config');
+
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  const program = {
+    directory: __dirname,
+    browserslist: ['> 1%', 'last 2 versions', 'IE >= 9'],
+  };
+
+  return generateBabelConfig(program, stage).then((babelConfig) => {
+    config.removeLoader('js').loader('js', {
+      test: /\.jsx?$/,
+      exclude: modulePath => (
+        /node_modules/.test(modulePath) &&
+        !/node_modules\/(swiper|dom7)/.test(modulePath)
+      ),
+      loader: 'babel',
+      query: babelConfig,
+    });
+  });
+};
+
 module.exports = {
   siteMetadata: {
     title: 'SimeonSmith.me',

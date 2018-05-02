73235ccbb9e656039f9fe7b7d06cdc95e45bbe8b
ssmith-wombatweb <ssmith@wombatweb.us>
2018-05-01 21:11:01 -0700

    Fixed issue with layout on 404 page.

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
 
-const NotFoundPage = () => (
-  <div>
-    <h1>Page Not Found</h1>
-    <p>
-      Oh No! You've wondered off and gotten lost. Please use choose from the pages in the menu to
-      find your way back.
-    </p>
-  </div>
+const NotFoundPage = ({ title }) => (
+  <main>
+    <Helmet>
+      <title>{title} - Home</title>
+    </Helmet>
+    <div className="page-content">
+      <h1>Page Not Found</h1>
+      <p>
+        Oh No! You've wondered off and gotten lost. Please choose from the pages in the menu to
+        find your way back.
+      </p>
+    </div>
+  </main>
 );
+
+NotFoundPage.propTypes = {
+  title: PropTypes.string,
+};
+
+NotFoundPage.defaultProps = {
+  title: 'SimeonSmith.me',
+};
 
 export default NotFoundPage;

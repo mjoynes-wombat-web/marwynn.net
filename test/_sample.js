a3ef29383e40966fde0f1bd164fd452088a5c3cf
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-03 10:59:42 -0700

    Made failing test to demonstrate and front-end-development meetup.

---
+++ test/_sample.js
@@ -0,0 +1,6 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+describe('This is an example test to show how it runs on Netlify.', () => {
+  it('Should succeed!', () => expect(false).to.equal(true));
+});

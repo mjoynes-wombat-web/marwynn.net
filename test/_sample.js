18d954985f75ad9f6f339de1a33457845638ed67
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-03 21:06:46 -0700

    Removed bablerc and reverted imports to common js in testing to stop babelrc from overwriting gatsby babel config.

--- test/_sample.js
+++ test/_sample.js
@@ -1,6 +1,6 @@
-import { expect } from 'chai';
-import { describe, it } from 'mocha';
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
 
 describe('This is an example test to show how it runs on Netlify.', () => {
-  it('Should succeed!', () => expect(false).to.equal(true));
+  it('Should succeed!', () => expect(true).to.equal(true));
 });

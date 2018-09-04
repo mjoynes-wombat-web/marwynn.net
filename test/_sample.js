3f96223b073904fb65fbfb39eb04d9181e06e74a
ssmith-wombatweb <ssmith@wombatweb.us>
2018-09-03 21:00:11 -0700

    Remove test error for initial build.

--- test/_sample.js
+++ test/_sample.js
@@ -2,5 +2,5 @@ import { expect } from 'chai';
 import { describe, it } from 'mocha';
 
 describe('This is an example test to show how it runs on Netlify.', () => {
-  it('Should succeed!', () => expect(false).to.equal(true));
+  it('Should succeed!', () => expect(true).to.equal(true));
 });
